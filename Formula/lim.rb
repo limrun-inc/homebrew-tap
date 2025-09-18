727f40320f8cf6b9dfac14d2da5f301b2740e545
limrunbot <karl@limrun.com>
2025-09-18 18:18:24 +0300

    Update lim.rb

--- Formula/lim.rb
+++ Formula/lim.rb
@@ -17,13 +17,13 @@ class Lim < Formula
     if Hardware::CPU.arm?
       depends_on "limrun-inc/tap/scrcpy"
 
-      url "https://github.com/limrun-inc/homebrew-tap/releases/download/#{version}/lim-darwin-arm64"
+      url "https://github.com/limrun-inc/lim/releases/download/#{version}/lim-darwin-arm64"
       sha256 "d10b6ef946e35d73f110889590c0604e7b3a2c2de869d8f5f905ab476db73c98" # replace_with_darwin_arm64_sha256
     else
       # scrcpy does not yet publish static builds for darwin-amd64
       depends_on "scrcpy"
 
-      url "https://github.com/limrun-inc/homebrew-tap/releases/download/#{version}/lim-darwin-amd64"
+      url "https://github.com/limrun-inc/lim/releases/download/#{version}/lim-darwin-amd64"
       sha256 "74875caf9cbe706311f5ac724e182ea2d8435df9982b7b5a808dbb22d8192a0b" # replace_with_darwin_amd64_sha256
     end
   end
@@ -33,12 +33,12 @@ class Lim < Formula
       # scrcpy does not yet publish static builds for linux-arm64
       depends_on "scrcpy"
 
-      url "https://github.com/limrun-inc/homebrew-tap/releases/download/#{version}/lim-linux-arm64"
+      url "https://github.com/limrun-inc/lim/releases/download/#{version}/lim-linux-arm64"
       sha256 "16471aaae161b90ef8f959430e852a3e748b09df6d09aaf0ff44db0406faad7a" # replace_with_linux_arm64_sha256
     else
       depends_on "limrun-inc/tap/scrcpy"
 
-      url "https://github.com/limrun-inc/homebrew-tap/releases/download/#{version}/lim-linux-amd64"
+      url "https://github.com/limrun-inc/lim/releases/download/#{version}/lim-linux-amd64"
       sha256 "1302287aef4e095d4611141d55a28065252e71359847fe1ee98e44530649e27b" # replace_with_linux_amd64_sha256
     end
   end
